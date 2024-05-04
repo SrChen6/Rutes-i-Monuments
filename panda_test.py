d5be6ee510a7df293d93a6818283e49a30f67015
SrChen6 <haokangchen6@gmail.com>
2024-05-04 13:35:22 +0200

    imported a small csv, testing its functionalities

--- panda_test.py
+++ panda_test.py
@@ -1,11 +1,4 @@
 import pandas as pd
 from yogi import read
-df = pd.DataFrame([], columns=['x', 'y', 't', 's'])
-series = pd.Series([9.0,8.0,7.0,6.0], index=df.columns)
-df.loc[len(df.index)] = series
-print(df)
-while True:
-    df.loc[len(df.index)] = [1.0, 2.0, 3.0, 4.0]
-    if len(df.index) > 100:
-        df.to_csv("data.csv", sep=" ", header=False, index=False)
-        break
+df = pd.read_csv('data.csv', sep=" ", header=0)
+print(df['x'])
