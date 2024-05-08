6aee7a87695a2a648186814a66cf4aea5613724c
SrChen6 <haokangchen6@gmail.com>
2024-05-08 12:04:02 +0200

    added the number of cluster to the csv

--- clustering.py
+++ clustering.py
@@ -3,6 +3,8 @@ from pandas import DataFrame
 from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 
+from raw_graph import plot_csv
+
 def cluster(df: DataFrame, n: int)-> DataFrame:
     """Donat un dataframe amb coordenades i un enter n, retorna les 
     coordenades del cluster amb n coordenades"""
@@ -11,16 +13,23 @@ def cluster(df: DataFrame, n: int)-> DataFrame:
     points = list(zip(x_lst, y_lst))
     kmeans = KMeans(n_clusters=n, random_state=0, n_init="auto").fit(points) #el 100 és subject to change
     labels = kmeans.labels_ #Gives a number for every cluster
+    df['cluster'] = labels
+    df.to_csv("ebre_clusters.csv", index=False, sep=" ")
     x_clustered, y_clustered = zip(*kmeans.cluster_centers_)
     plt.plot(x_clustered, y_clustered, 'ko', markersize=1)
-    df_clustered = pd.DataFrame([[x_clustered], [y_clustered]], columns=['x', 'y']) #TODO: ahora mismo añade las listas en una fila. Se quiere que se añadan en una columna.
+    df_clustered = pd.DataFrame()
+    df_clustered['x'] = x_clustered
+    df_clustered['y'] = y_clustered
+    df_clustered['num'] = [i for i in range(n)]
+    plot_csv(df)
+    plot_csv(df_clustered)
     return df_clustered
 
 def main() -> None:
-    df = pd.read_csv("ebre.csv", sep=" ", header=0)
+    df = pd.read_csv("ebre2.csv", sep=" ", header=0)
     new_df = cluster(df, 100)
     print("dataframe returned")
-    new_df.to_csv("test.csv", sep=" ", index=False)
+    new_df.to_csv("clusters.csv", sep=" ", index=False)
     
 
 if __name__ == "__main__":
