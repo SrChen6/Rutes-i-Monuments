39d2e36e781d28a7aaaf0d37521f9cfe188c15e0
SrChen6 <haokangchen6@gmail.com>
2024-05-12 13:23:31 +0200

    Graph is functional (not correct tho)

--- clustering.py
+++ clustering.py
@@ -1,53 +1,34 @@
-from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 import csv
+import plotter
 
-from raw_graph import plot_csv
-
-def cluster(n: int)-> list[tuple[int, int]]:
-    """Donat un dataframe amb coordenades i un enter n, retorna les 
-    coordenades del cluster amb n coordenades"""
-    x: list[float] = []
-    y: list[float] = []
-    with open('ebre3.csv', newline='') as f:
+def cluster(n: int)-> list[tuple[float, float]]:
+    """Donat un dataframe amb coordenades i un enter n, afegeix una columna
+    al dataframe corresponent al label del cluster"""
+    with open('ebre3.csv', 'r', newline='') as f:
         reader = csv.reader(f)
         data = list(reader)
-    # de martí: jo aquí faria
-    # coords = [(x, y) for x, y, _, _ in data]
-    for punt in data:
-        x.append(float(punt[0]))
-        y.append(float(punt[1]))
-    plt.scatter(x, y, color='black', s=1)
-    plt.show()
-    coords = list(zip(x, y))
+    coords = [(float(x), float(y)) for x, y, _ in data]
+
+    # x, y = zip(*coords)
+    # plotter.plot_lists(x, y)
+
     kmeans = KMeans(n_clusters=n, random_state=0, n_init="auto").fit(coords)
-    print(kmeans.cluster_centers_)
-    x_cl, y_cl = zip(*kmeans.cluster_centers_)
-    plt.scatter(x_cl, y_cl, color='black', s=1)
-    plt.show()
 
+    # for i in range(len(data)):
+    #     data[i].append(kmeans.labels_[i])
+    # with open('ebre_clusters.csv', 'w', newline='') as f:
+    #     writer =csv.writer(f)
+    #     writer.writerows(data)
 
-    # x_lst = df['x'].tolist() #TODO: Veure si val la pena el .tolist()
-    # y_lst = df['y'].tolist()
-    # points = list(zip(x_lst, y_lst))
-    # kmeans = KMeans(n_clusters=n, random_state=0, n_init="auto").fit(points) #el 100 és subject to change
-    # labels = kmeans.labels_ #Gives a number for every cluster
-    # df['cluster'] = labels
-    # df.to_csv("ebre_clusters.csv", index=False, sep=" ")
-    # x_clustered, y_clustered = zip(*kmeans.cluster_centers_)
-    # plt.plot(x_clustered, y_clustered, 'ko', markersize=1)
-    # df_clustered = pd.DataFrame()
-    # df_clustered['x'] = x_clustered
-    # df_clustered['y'] = y_clustered
-    # df_clustered['num'] = [i for i in range(n)]
-    # plot_csv(df)
-    # plot_csv(df_clustered)
-    # return df_clustered
+    # x_cl, y_cl = zip(*kmeans.cluster_centers_)
+    # plotter.plot_lists(x_cl, y_cl)
+    return kmeans.cluster_centers_.tolist()
+    
 
 def main() -> None:
     new_df = cluster(100)
     print("clusters returned")
-    print(new_df)
 
 if __name__ == "__main__":
     main()
--- path_builder.py
+++ path_builder.py
@@ -1,6 +1,91 @@
+from haversine import haversine
+import networkx as nx
+from sklearn.cluster import KMeans
+import csv
 from matplotlib import pyplot as plt
 
-def main() -> None:
-    coord = pd.read_csv("ebre_clusters.csv", sep=" ", header=0)
-    clust = pd.read_csv("clusters.csv", sep=" ", header=0)
-    #TODO: crear una funció que, donats unes coords i uns clusters, retorni els camins que uneixen clusters
+
+from clustering import cluster
+
+from random import randint
+import yogi
+
+
+Point = tuple[float, float]
+Path = list[tuple[Point, Point]]
+
+
+def distance(p1: Point, p2: Point) -> float:
+    """Returns the distance between two points on Earth expressed in
+    spherical coordinates (longitude, latitude)."""
+    return haversine((p1[1], p1[0]), (p2[1], p2[0]))
+
+# TODO: capçalera subject to change, l'estructura de les dades pot canviar
+def cluster_paths(clust_center: list[Point]) -> nx.Graph:
+    """
+    L'entrada és una llista de tuples corresponents als centres dels clusters
+    Es retorna el graf amb els camins
+    """
+    #Afegir nodes
+    G = nx.Graph()
+    for i in range(len(clust_center)):
+        G.add_node(i, pos=clust_center[i])
+
+    #Obrir CSV
+    with open('ebre_clusters.csv', 'r', newline='') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+
+    #Buscar Arestes
+    prev_seg = -1
+    prev_label = -1
+    for _, _, s, c in data:
+        if prev_seg == int(s) and prev_label != int(c):
+            G.add_edge(prev_label, int(c))
+        prev_seg = int(s)
+        prev_label = int(c)
+    
+    pos = nx.get_node_attributes(G, 'pos')
+    nx.draw(G, pos, node_color='red', node_size=2)
+    plt.show()
+    return G
+
+def create_graph_experimental(data: list[tuple[Point, int]], n: int) -> nx.Graph:
+    """
+    L'entrada és una llista de tuples i el nombre de clusters.
+    En cada tupla, el primer element és un punt, el segon és l'ID del seu segment.
+
+    Fa el clustering de tots els punts.
+    Retorna una llista de camins entre clusters.
+    """
+    paths = clustering()
+
+def create_graph(cluster_paths: list[list[Point]], filename: str) -> nx.Graph:
+    """
+    Entrada: Una llista de camins
+    
+    Retorna un graf amb els clusters com a nodes i les connexions entre clusters
+    com a arestes.
+    """
+    graph = nx.Graph()
+    for path in cluster_paths:
+        for i in range(len(path) - 1):
+            graph.add_edge(path[i], path[i+1], dist = distance(path[i], path[i+1]))
+    return graph
+
+    ...
+
+def simplify_graph(graph: nx.Graph) -> nx.Graph:
+    """
+    Simplifica el graf.
+    """
+
+
+def testing() -> None:
+    """
+    Prova les funcions del mòdul.
+    """
+    cluster_paths(cluster(100))
+    ...
+
+if __name__ == "__main__": testing()
